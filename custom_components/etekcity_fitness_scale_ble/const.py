b1605e2317968834438196497fa11c44b27ccee7
Ron <ron.benyosef@outlook.com>
2026-02-19 00:15:10 +0000

    Add scale model enumeration and detection patterns to constants

--- custom_components/etekcity_fitness_scale_ble/const.py
+++ custom_components/etekcity_fitness_scale_ble/const.py
@@ -1,5 +1,7 @@
 """Constants for the etekcity_fitness_scale_ble integration."""
 
+from enum import StrEnum
+
 DOMAIN = "etekcity_fitness_scale_ble"
 
 # Legacy constants (used for migration from v1)
@@ -9,6 +11,27 @@ CONF_HEIGHT = "height"
 CONF_BIRTHDATE = "birthdate"
 CONF_FEET = "feet"
 CONF_INCHES = "inches"
+
+CONF_SCALE_MODEL = "scale_model"
+
+
+class ScaleModel(StrEnum):
+    """Scale model enumeration."""
+
+    ESF551 = "ESF-551"
+    ESF24 = "ESF-24"
+
+
+# Detection patterns for different scale models
+SCALE_DETECTION_PATTERNS = {
+    ScaleModel.ESF24: {
+        "local_name": "QN-Scale1"
+    },
+    ScaleModel.ESF551: {
+        "local_name_pattern": "Etekcity *Fitness *Scale*",
+        "manufacturer_id": 1744
+    }
+}
 
 # Multi-user constants (v2+)
 CONF_SCALE_DISPLAY_UNIT = "scale_display_unit"
