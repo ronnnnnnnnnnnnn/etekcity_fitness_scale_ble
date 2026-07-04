e67cb754231b9b476e1bbb95c214e12aa1dc937b
Ron <ron.benyosef@outlook.com>
2026-07-05 00:30:09 +0100

    formatting

--- custom_components/etekcity_fitness_scale_ble/config_flow.py
+++ custom_components/etekcity_fitness_scale_ble/config_flow.py
@@ -715,7 +715,9 @@ class ScaleConfigFlow(ConfigFlow, domain=DOMAIN):
             return await self.async_step_add_first_user()
 
         current_addresses = self._async_current_ids()
-        for discovery_info in async_discovered_service_info(self.hass, connectable=False):
+        for discovery_info in async_discovered_service_info(
+            self.hass, connectable=False
+        ):
             address = discovery_info.address
             if address in current_addresses or address in self._discovered_devices:
                 continue
